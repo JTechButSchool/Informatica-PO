2b8af77f7c062738fad808b0af808c8127099225
JTechButSchool <1010182@leerling.tabor.nl>
2024-10-24 12:29:16 +0000

    javascript update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
   var items = document.querySelectorAll(".reveal");
-  for (let i=0;i<items.length;i++) {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {  
+  var iitems = document.querySelectorAll(".ireveal");
+  for (let i = 0; i < items.length; i++) {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
       items[i].classList.add("active");
     } else {
       items[i].classList.remove("active");
+    }
+  }
+  for (let i = 0; i < iitems.length; i++) {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      iitems[i].classList.add("active");
+    } else {
+      iitems[i].classList.remove("active");
     }
   }
 }
